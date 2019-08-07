428845126495838c42b7ee3710a83019c2c372d1
Gabriel D. Karaffa <gk71pro@gmail.com>
2019-08-07 10:58:48 -0400

    Commit point

--- src/com/gkaraffa/polyhedron/Thrower.java
+++ src/com/gkaraffa/polyhedron/Thrower.java
@@ -3,7 +3,7 @@ package com.gkaraffa.polyhedron;
 import java.util.*;
 
 public class Thrower {
-  static public int throwBestMOfN(int iM, int iN, Die die) throws IllegalArgumentException {
+  public static int throwBestMOfN(int iM, int iN, Die die) throws IllegalArgumentException {
     if (iM >= iN) {
       throw new IllegalArgumentException("M must be less than N");
     }
@@ -19,15 +19,13 @@ public class Thrower {
     int rightExt = iN - 1;
     int leftExt = (iN - iM) - 1;
     for (int index = rightExt; index > leftExt; index--) {
-      System.out.println("i: " + iArray[index]);
       iAccum += iArray[index];
     }
-    System.out.println("\n");
 
     return (iAccum);
   }
   
-  static public int throwNTimes(int iN, Die die) {
+  public static int throwNTimes(int iN, Die die) {
     int iAccum = 0;
     for (int index = 0; index < iN; index++) {
       iAccum += die.roll();
@@ -35,13 +33,8 @@ public class Thrower {
     
     return iAccum;
   }
-  
-  /*
-  static public void getDistribution(Die die, int size) {
-  }
-  */
-  
-  static public Results throwRoll(Roll roll) {
+    
+  public static Results throwRoll(Roll roll) {
     int tosses = roll.getTosses();
     int aggreg = 0;
     int resAry[] = new int[tosses];
